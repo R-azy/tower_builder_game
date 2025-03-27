89eb6dde4dbae215ae8c29219ccb9a382c460613
R-azy <muhammedrazy1798@gmail.com>
2025-03-27 16:57:46 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="bg-[url(./src/assets/image4.png)] bg-cover h-screen flex justify-center items-center">
+      <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-lg w-500 max-w-2xl text-center">
+        
+        🏗️ Tower Builder  Overview Stack blocks with perfect timing to build
+        the tallest, most stable tower! Gameplay: Tap to drop moving blocks onto
+        the Goal: Keep stacking the towers in a stable order. Fun Factor: Easy
+        to play, tricky to master  how high can you build?
+      </div>
+    </div>
+  );
+
+}
+
+export default Home
